f2721566a4bdcebc1bbe23a8b968b5108df89f6a
Gurdeep <gurdeepsingh@cssoftsolutions.com>
2022-02-10 12:15:58 +0530

    i18n configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-    include Pagy::Backend
+  include Pagy::Backend
+  before_action :set_locale
+
+  private
+  def set_locale
+      if %w(ja en).include?(params[:locale])
+      I18n.locale = params[:locale]
+      else
+        I18n.locale = "ja"
+      end
+  end
 end
