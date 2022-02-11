15a391f5904f2cae8109f771be14db61a141fae6
Gurdeep <gurdeepsingh@cssoftsolutions.com>
2022-02-11 19:11:47 +0530

    Administrator Dashboard,create new User,Roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  enum role: { Administrator: 'Administrator', ClinicUser: 'ClinicUser'}, _default: " "
+  after_initialize :set_default_role, :if => :new_record?
+              
+  def set_default_role
+    self.role ||= :ClinicUser
+  end
 end
