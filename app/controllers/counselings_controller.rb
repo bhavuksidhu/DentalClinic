f4b9c098222d93a00fae593aa4ad14776be53b25
Gurdeep <gurdeepsingh@cssoftsolutions.com>
2022-03-09 12:57:05 +0530

    added aggregation_basic_information in counselling section

--- app/controllers/counselings_controller.rb
+++ app/controllers/counselings_controller.rb
@@ -30,8 +30,32 @@ class CounselingsController < ApplicationController
     @counseling = Counseling.includes(:oral_types).find(params[:id])
   end 
 
+
   def aggregation_basic_information
-    
+      @aggregation_period=params[:aggregation_period]
+
+      if @aggregation_period == "6Months"
+        @aggregation_basic_information=Counseling.where(created_at: 6.months.ago..Time.now)
+
+      elsif @aggregation_period == "1Year"
+        @aggregation_basic_information=Counseling.where(created_at: 1.year.ago..Time.now)
+
+      elsif @aggregation_period == "7Days"
+        @aggregation_basic_information=Counseling.where(created_at: 7.days.ago..Time.now)
+
+      elsif @aggregation_period == "15Days"
+        @aggregation_basic_information=Counseling.where(created_at: 15.days.ago..Time.now)
+
+      elsif @aggregation_period == "30Days"
+        @aggregation_basic_information=Counseling.where(created_at: 30.days.ago..Time.now)
+
+      elsif @aggregation_period == "Today"
+        @aggregation_basic_information=Counseling.where(created_at: Time.now)
+        
+      else
+        @aggregation_period = "All"
+        @aggregation_basic_information=Counseling.all
+      end
   end
 
 
